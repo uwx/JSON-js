1953b816020a41387fe383821e8bc5ae7ca12f53
chrishansen69 <cadrekucra@gmx.com>
2017-08-16 12:33:19 -0300

    add test system

---
+++ test.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const json_parse = require('./json_parse');
+const fs = require('fs-extra');
+
+fs.diveSync('./test-cases').filter(e => e.endsWith('.json')).forEach(e => {
+  console.log(json_parse(fs.readFileSync(e, 'utf8')));
+  //console.log('success:', e);
+});
