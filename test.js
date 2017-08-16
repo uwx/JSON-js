ca39a0445b8b6c8dcbb968c7de9d0fb89a354505
chrishansen69 <cadrekucra@gmx.com>
2017-08-16 19:39:23 -0300

    fix tests attempting to read from .json instead of .lmjson

--- test.js
+++ test.js
@@ -3,7 +3,7 @@
 const json_parse = require('./json_parse');
 const fs = require('fs-extra');
 
-fs.diveSync('./test-cases').filter(e => e.endsWith('.json')).forEach(e => {
+fs.diveSync('./test-cases').filter(e => e.endsWith('.lmjson')).forEach(e => {
   console.log(json_parse(fs.readFileSync(e, 'utf8')));
   //console.log('success:', e);
 });
